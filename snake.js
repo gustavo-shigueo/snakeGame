b7c15fc07ad900d1d54a6d626419488177bda4ea
gustavo-shigueo <gustavo.gsmn@gmail.com>
2020-10-15 19:28:49 -0300

    Commenting new code

--- snake.js
+++ snake.js
@@ -2,10 +2,10 @@ class Snake{
   constructor(){
     this.x = 10 * grid
     this.y = 10 * grid
-    
+
     // Movement direction: [xDirection, yDirection]
     this.dir = [1, 0]
-    
+
     // Snake starts with a size of 3 (head + 2 tail pieces)
     this.tail = [
       [this.x, this.y],
@@ -20,12 +20,12 @@ class Snake{
       'ArrowRight': [ 1,  0]
     }
   }
-  
+
   update(){
     // Moves the tail
     this.tail.unshift([this.x, this.y])
     this.tail.splice(-1)
-    
+
     // Moves the head
     this.x += this.dir[0] * grid
     this.y += this.dir[1] * grid
@@ -39,38 +39,38 @@ class Snake{
 
     // Prevents death by changing directions very quickly
     if (this.tail[0][0] === this.x + xD * grid && this.tail[0][1] === this.y + yD * grid) return
-    
+
     // Prevents player from going backwards and dying
     if ((xD === 0 && this.dir[1] === 0) || (yD === 0 && this.dir[0] === 0)) return this.dir = [xD, yD]
   }
-  
+
   show(){
     // Draws the snake
     fill(255)
     rect(this.x, this.y, grid)
     for(const piece of this.tail) rect(piece[0], piece[1], grid)
   }
-  
+
   eat(fObj){
     // Checks if food and the snake's head have the same position
     if(dist(this.x, this.y, fObj.x, fObj.y) < grid) return true
     return
   }
-  
+
   increaseTail(){
     // Increases snake tail
     return this.tail.push(this.tail[0])
   }
-  
+
   death(){
     // Checks if player hit a wall
     if(this.x <= -grid || this.y <= -grid || this.x >= w || this.y >= h) return true
-    
+
     // Checks if player hit the tail
     for(const piece of this.tail) {
       if(this.x === piece[0] && this.y === piece[1]) return true
     }
-    
+
     // Player didn't die
     return
   }
--- swipe.js
+++ swipe.js
@@ -1,10 +1,13 @@
 class Swipe{
     constructor(element) {
+        // Where the player touches the screen
         this.xDown = null
         this.yDown = null
 
+        // Element listening for swipes
         this.element = typeof(element) === 'string' ? document.querySelector(element) : element
 
+        // Getting the location of the touch
         this.element.addEventListener('touchstart', function(event) {
             this.xDown = event.touches[0].clientX
             this.yDown = event.touches[0].clientY
@@ -34,15 +37,20 @@ class Swipe{
     handleTouchMove(event) {
         if (!this.xDown || !this.yDown) return
 
+        // Where the player releases the screen
         let xUp = event.touches[0].clientX
         let yUp = event.touches[0].clientY
 
+        // Distance between the Down and Up variables
         this.xDiff = this.xDown - xUp
         this.yDiff = this.yDown - yUp
 
+        /* Checks if the swipe was horizontal or vertical, then decides the direction
+           in the proper axis using the Diff variables */
         if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) this.xDiff > 0 ? this.onLeft() : this.onRight()
         else this.yDiff > 0 ? this.onUp() : this.onDown()
 
+        // Reset
         this.xDown = null
         this.yDown = null
     }
